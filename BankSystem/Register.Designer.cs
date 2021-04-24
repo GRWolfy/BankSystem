18a68eb4d2a74cfa33d39b64c6f3bf1ad86274da
grw99 <granthreggwolf@gmail.com>
2021-04-24 13:10:24 +0800

    Update 1.3

--- BankSystem/Register.Designer.cs
+++ BankSystem/Register.Designer.cs
@@ -53,12 +53,14 @@ namespace BankSystem
          this.label9 = new System.Windows.Forms.Label();
          this.txtConfirmPassword = new System.Windows.Forms.TextBox();
          this.label10 = new System.Windows.Forms.Label();
+         this.labelConfirmPassword = new System.Windows.Forms.Label();
+         this.labelEmail = new System.Windows.Forms.Label();
          this.SuspendLayout();
          // 
          // label1
          // 
          this.label1.AutoSize = true;
-         this.label1.Location = new System.Drawing.Point(98, 118);
+         this.label1.Location = new System.Drawing.Point(60, 114);
          this.label1.Name = "label1";
          this.label1.Size = new System.Drawing.Size(61, 13);
          this.label1.TabIndex = 4;
@@ -67,7 +69,7 @@ namespace BankSystem
          // label2
          // 
          this.label2.AutoSize = true;
-         this.label2.Location = new System.Drawing.Point(98, 208);
+         this.label2.Location = new System.Drawing.Point(60, 204);
          this.label2.Name = "label2";
          this.label2.Size = new System.Drawing.Size(29, 13);
          this.label2.TabIndex = 5;
@@ -76,7 +78,7 @@ namespace BankSystem
          // label3
          // 
          this.label3.AutoSize = true;
-         this.label3.Location = new System.Drawing.Point(498, 118);
+         this.label3.Location = new System.Drawing.Point(420, 76);
          this.label3.Name = "label3";
          this.label3.Size = new System.Drawing.Size(48, 13);
          this.label3.TabIndex = 6;
@@ -85,7 +87,7 @@ namespace BankSystem
          // label4
          // 
          this.label4.AutoSize = true;
-         this.label4.Location = new System.Drawing.Point(511, 208);
+         this.label4.Location = new System.Drawing.Point(433, 166);
          this.label4.Name = "label4";
          this.label4.Size = new System.Drawing.Size(35, 13);
          this.label4.TabIndex = 7;
@@ -94,7 +96,7 @@ namespace BankSystem
          // label5
          // 
          this.label5.AutoSize = true;
-         this.label5.Location = new System.Drawing.Point(101, 163);
+         this.label5.Location = new System.Drawing.Point(63, 159);
          this.label5.Name = "label5";
          this.label5.Size = new System.Drawing.Size(58, 13);
          this.label5.TabIndex = 9;
@@ -123,7 +125,7 @@ namespace BankSystem
          // label6
          // 
          this.label6.AutoSize = true;
-         this.label6.Location = new System.Drawing.Point(98, 342);
+         this.label6.Location = new System.Drawing.Point(60, 338);
          this.label6.Name = "label6";
          this.label6.Size = new System.Drawing.Size(45, 13);
          this.label6.TabIndex = 15;
@@ -131,43 +133,45 @@ namespace BankSystem
          // 
          // txtFirstName
          // 
-         this.txtFirstName.Location = new System.Drawing.Point(198, 70);
+         this.txtFirstName.Location = new System.Drawing.Point(160, 66);
          this.txtFirstName.Name = "txtFirstName";
          this.txtFirstName.Size = new System.Drawing.Size(183, 20);
          this.txtFirstName.TabIndex = 0;
          // 
          // txtAddress
          // 
-         this.txtAddress.Location = new System.Drawing.Point(565, 115);
+         this.txtAddress.Location = new System.Drawing.Point(487, 73);
          this.txtAddress.Name = "txtAddress";
          this.txtAddress.Size = new System.Drawing.Size(183, 20);
          this.txtAddress.TabIndex = 1;
          // 
          // txtAge
          // 
-         this.txtAge.Location = new System.Drawing.Point(198, 205);
+         this.txtAge.Location = new System.Drawing.Point(160, 201);
          this.txtAge.Name = "txtAge";
          this.txtAge.Size = new System.Drawing.Size(183, 20);
          this.txtAge.TabIndex = 2;
+         this.txtAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAge_KeyPress);
          // 
          // txtUsername
          // 
-         this.txtUsername.Location = new System.Drawing.Point(198, 160);
+         this.txtUsername.Location = new System.Drawing.Point(160, 156);
          this.txtUsername.Name = "txtUsername";
          this.txtUsername.Size = new System.Drawing.Size(183, 20);
          this.txtUsername.TabIndex = 3;
          // 
          // txtMobileNo
          // 
-         this.txtMobileNo.Location = new System.Drawing.Point(565, 160);
+         this.txtMobileNo.Location = new System.Drawing.Point(487, 118);
          this.txtMobileNo.Name = "txtMobileNo";
          this.txtMobileNo.Size = new System.Drawing.Size(183, 20);
          this.txtMobileNo.TabIndex = 8;
+         this.txtMobileNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMobileNo_KeyPress);
          // 
          // rbtnGenderOthers
          // 
          this.rbtnGenderOthers.AutoSize = true;
-         this.rbtnGenderOthers.Location = new System.Drawing.Point(317, 342);
+         this.rbtnGenderOthers.Location = new System.Drawing.Point(279, 338);
          this.rbtnGenderOthers.Name = "rbtnGenderOthers";
          this.rbtnGenderOthers.Size = new System.Drawing.Size(56, 17);
          this.rbtnGenderOthers.TabIndex = 12;
@@ -178,7 +182,7 @@ namespace BankSystem
          // rbtnGenderFemale
          // 
          this.rbtnGenderFemale.AutoSize = true;
-         this.rbtnGenderFemale.Location = new System.Drawing.Point(252, 342);
+         this.rbtnGenderFemale.Location = new System.Drawing.Point(214, 338);
          this.rbtnGenderFemale.Name = "rbtnGenderFemale";
          this.rbtnGenderFemale.Size = new System.Drawing.Size(59, 17);
          this.rbtnGenderFemale.TabIndex = 13;
@@ -189,7 +193,7 @@ namespace BankSystem
          // rbtnGenderMale
          // 
          this.rbtnGenderMale.AutoSize = true;
-         this.rbtnGenderMale.Location = new System.Drawing.Point(198, 342);
+         this.rbtnGenderMale.Location = new System.Drawing.Point(160, 338);
          this.rbtnGenderMale.Name = "rbtnGenderMale";
          this.rbtnGenderMale.Size = new System.Drawing.Size(48, 17);
          this.rbtnGenderMale.TabIndex = 14;
@@ -199,7 +203,7 @@ namespace BankSystem
          // 
          // txtLastName
          // 
-         this.txtLastName.Location = new System.Drawing.Point(198, 115);
+         this.txtLastName.Location = new System.Drawing.Point(160, 111);
          this.txtLastName.Name = "txtLastName";
          this.txtLastName.Size = new System.Drawing.Size(183, 20);
          this.txtLastName.TabIndex = 16;
@@ -207,7 +211,7 @@ namespace BankSystem
          // label7
          // 
          this.label7.AutoSize = true;
-         this.label7.Location = new System.Drawing.Point(98, 73);
+         this.label7.Location = new System.Drawing.Point(60, 69);
          this.label7.Name = "label7";
          this.label7.Size = new System.Drawing.Size(60, 13);
          this.label7.TabIndex = 17;
@@ -215,15 +219,16 @@ namespace BankSystem
          // 
          // txtEmail
          // 
-         this.txtEmail.Location = new System.Drawing.Point(565, 205);
+         this.txtEmail.Location = new System.Drawing.Point(487, 163);
          this.txtEmail.Name = "txtEmail";
          this.txtEmail.Size = new System.Drawing.Size(183, 20);
          this.txtEmail.TabIndex = 18;
+         this.txtEmail.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEmail_KeyPress);
          // 
          // label8
          // 
          this.label8.AutoSize = true;
-         this.label8.Location = new System.Drawing.Point(498, 163);
+         this.label8.Location = new System.Drawing.Point(420, 121);
          this.label8.Name = "label8";
          this.label8.Size = new System.Drawing.Size(51, 13);
          this.label8.TabIndex = 19;
@@ -231,7 +236,7 @@ namespace BankSystem
          // 
          // txtPassword
          // 
-         this.txtPassword.Location = new System.Drawing.Point(198, 253);
+         this.txtPassword.Location = new System.Drawing.Point(160, 249);
          this.txtPassword.Name = "txtPassword";
          this.txtPassword.Size = new System.Drawing.Size(183, 20);
          this.txtPassword.TabIndex = 20;
@@ -240,7 +245,7 @@ namespace BankSystem
          // label9
          // 
          this.label9.AutoSize = true;
-         this.label9.Location = new System.Drawing.Point(98, 253);
+         this.label9.Location = new System.Drawing.Point(60, 249);
          this.label9.Name = "label9";
          this.label9.Size = new System.Drawing.Size(56, 13);
          this.label9.TabIndex = 21;
@@ -248,26 +253,49 @@ namespace BankSystem
          // 
          // txtConfirmPassword
          // 
-         this.txtConfirmPassword.Location = new System.Drawing.Point(198, 297);
+         this.txtConfirmPassword.Location = new System.Drawing.Point(160, 293);
          this.txtConfirmPassword.Name = "txtConfirmPassword";
          this.txtConfirmPassword.Size = new System.Drawing.Size(183, 20);
          this.txtConfirmPassword.TabIndex = 22;
          this.txtConfirmPassword.UseSystemPasswordChar = true;
+         this.txtConfirmPassword.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtConfirmPassword_KeyUp);
          // 
          // label10
          // 
          this.label10.AutoSize = true;
-         this.label10.Location = new System.Drawing.Point(98, 300);
+         this.label10.Location = new System.Drawing.Point(60, 296);
          this.label10.Name = "label10";
          this.label10.Size = new System.Drawing.Size(94, 13);
          this.label10.TabIndex = 23;
          this.label10.Text = "Confirm Password:";
+         // 
+         // labelConfirmPassword
+         // 
+         this.labelConfirmPassword.AutoSize = true;
+         this.labelConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.labelConfirmPassword.Location = new System.Drawing.Point(387, 297);
+         this.labelConfirmPassword.Name = "labelConfirmPassword";
+         this.labelConfirmPassword.Size = new System.Drawing.Size(20, 16);
+         this.labelConfirmPassword.TabIndex = 25;
+         this.labelConfirmPassword.Text = "....";
+         // 
+         // labelEmail
+         // 
+         this.labelEmail.AutoSize = true;
+         this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.labelEmail.Location = new System.Drawing.Point(676, 167);
+         this.labelEmail.Name = "labelEmail";
+         this.labelEmail.Size = new System.Drawing.Size(20, 16);
+         this.labelEmail.TabIndex = 26;
+         this.labelEmail.Text = "....";
          // 
          // Register
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.ClientSize = new System.Drawing.Size(800, 450);
+         this.Controls.Add(this.labelEmail);
+         this.Controls.Add(this.labelConfirmPassword);
          this.Controls.Add(this.label10);
          this.Controls.Add(this.txtConfirmPassword);
          this.Controls.Add(this.label9);
@@ -324,5 +352,7 @@ namespace BankSystem
       private System.Windows.Forms.Label label9;
       private System.Windows.Forms.TextBox txtConfirmPassword;
       private System.Windows.Forms.Label label10;
+      private System.Windows.Forms.Label labelConfirmPassword;
+      private System.Windows.Forms.Label labelEmail;
    }
 }
--- BankSystem/Register.cs
+++ BankSystem/Register.cs
@@ -28,32 +28,25 @@ namespace BankSystem
          else if (rbtnGenderFemale.Checked) { gender = "Female"; }
          else { gender = "Other"; }
 
-         /*if (txtPassword.Equals(txtConfirmPassword))
-         {*/
-            SqlCommand cmd = new SqlCommand("INSERT INTO RegistrationDb VALUES (@FirstName, @LastName, @Username, @Age, @Password, @Gender, @Address, @MobileNUmber, @Email)", con);
-            cmd.CommandType = CommandType.Text;
-            cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
-            cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
-            cmd.Parameters.AddWithValue("@Username", txtUsername.Text);
-            cmd.Parameters.AddWithValue("@Age", txtAge.Text);
-            cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
-            cmd.Parameters.AddWithValue("@Gender", gender);
-            cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
-            cmd.Parameters.AddWithValue("@MobileNumber", txtMobileNo.Text);
-            cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+         SqlCommand cmd = new SqlCommand("INSERT INTO RegistrationDb VALUES (@FirstName, @LastName, @Username, @Age, @Password, @Gender, @Address, @MobileNUmber, @Email)", con);
+         cmd.CommandType = CommandType.Text;
+         cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+         cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+         cmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+         cmd.Parameters.AddWithValue("@Age", txtAge.Text);
+         cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+         cmd.Parameters.AddWithValue("@Gender", gender);
+         cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+         cmd.Parameters.AddWithValue("@MobileNumber", txtMobileNo.Text);
+         cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
 
-            con.Open();
-            cmd.ExecuteNonQuery();
-            con.Close();
+         con.Open();
+         cmd.ExecuteNonQuery();
+         con.Close();
 
-            MessageBox.Show("Registratioin success.", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+         MessageBox.Show("Registratioin success.", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            ResetFormControls();
-         /*}
-         else
-         {
-            MessageBox.Show("Password don't match", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
-         }*/
+         ResetFormControls();
       }
 
       private void ResetFormControls()
@@ -63,9 +56,82 @@ namespace BankSystem
          txtUsername.Clear();
          txtAge.Clear();
          txtPassword.Clear();
-         //GENDER
+         txtConfirmPassword.Clear();
          txtAddress.Clear();
          txtEmail.Clear();
+      }
+
+      private void txtConfirmPassword_KeyUp(object sender, KeyEventArgs e)
+      {
+         if(txtPassword.Text.Equals(txtConfirmPassword.Text))
+         {
+            btnRegisterOK.Enabled = true;
+            labelConfirmPassword.Text = "Matched";
+         }
+         else
+         {
+            btnRegisterOK.Enabled = false;
+            labelConfirmPassword.Text = "Invalid password";
+         }
+      }
+
+      private void txtAge_KeyPress(object sender, KeyPressEventArgs e)
+      {
+         char ch = e.KeyChar;
+
+         if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+         {
+            e.Handled = true;
+            btnRegisterOK.Enabled = false;
+         }
+         else
+         {
+            
+            btnRegisterOK.Enabled = true;
+         }
+      }
+
+      private void txtMobileNo_KeyPress(object sender, KeyPressEventArgs e)
+      {
+         char ch = e.KeyChar;
+
+         if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+         {
+            e.Handled = true;
+            btnRegisterOK.Enabled = false;
+         }
+         else
+         {
+
+            btnRegisterOK.Enabled = true;
+         }
+      }
+
+      public bool IsValidEmail(string email)
+      {
+         try
+         {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+         }
+         catch
+         {
+            return false;
+         }
+      }
+
+      private void txtEmail_KeyPress(object sender, KeyPressEventArgs e)
+      {
+         if (IsValidEmail(txtEmail.Text))
+         {
+            btnRegisterOK.Enabled = true;
+            labelEmail.Text = "OK";
+         }
+         else
+         {
+            btnRegisterOK.Enabled = false;
+            labelEmail.Text = "Invalid email";
+         }
       }
    }
 }
